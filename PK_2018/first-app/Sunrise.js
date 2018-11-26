f7a1777903c506f6c1aad92d911082fa048e4fbe
Miron Pawlik <miron.pawlik@swmansion.com>
2018-11-26 14:50:38 +0100

    [PK] Use state to show coordinates

--- PK_2018/first-app/Sunrise.js
+++ PK_2018/first-app/Sunrise.js
@@ -3,17 +3,27 @@ import { Text } from "react-native";
 import { Location, Permissions } from "expo";
 
 export default class Sunrise extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userCoords: null
+    };
+  }
+
   async componentDidMount() {
     const { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status !== "granted") {
       console.warn("Permission to access location was denied");
     } else {
       const userLocation = await Location.getCurrentPositionAsync({});
-      console.warn("location", userLocation);
+      this.setState({ userCoords: userLocation.coords });
     }
   }
 
   render() {
-    return <Text>This is sample</Text>
+    if (this.state.userCoords) {
+      return <Text>{this.state.userCoords.longitude} - {this.state.userCoords.latitude}</Text>;
+    }
+    return <Text>waiting...</Text>;
   }
 }
